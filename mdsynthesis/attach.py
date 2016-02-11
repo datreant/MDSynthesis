fafa610191bc819467c3d18b711389fc56943441
David Dotson <dldotson@asu.edu>
2016-02-10 23:52:45 -0700

    Group also needs to get a data limb.
    Now it does.

--- mdsynthesis/attach.py
+++ mdsynthesis/attach.py
@@ -2,11 +2,13 @@
 Limb attachments for :mod:`mdsynthesis` classes.
 
 """
-
-from .treants import Sim
+from datreant.core.treants import Group
 from datreant.core.collections import CollectionBase
 from datreant.data.limbs import Data
 from datreant.data.agglimbs import MemberData
 
+from .treants import Sim
+
 Sim._attach_limb_class(Data)
+Group._attach_limb_class(Data)
 CollectionBase._attach_agglimb_class(MemberData)
