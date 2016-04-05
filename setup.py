a77baf05238862091f5f9b9567d7752d14d8fd7f
David Dotson <dotsdl@gmail.com>
2016-04-04 18:45:43 -0700

    Release 0.6.0.

--- setup.py
+++ setup.py
@@ -10,9 +10,21 @@ For a basic installation just type the command::
 from setuptools import setup, find_packages
 
 setup(name='mdsynthesis',
-      version='0.6.0-dev',
-      maintainer='David Dotson', 
-      maintainer_email='dotsdl@gmail.com',
+      version='0.6.0',
+      description='a persistence engine for molecular dynamics data',
+      author='David Dotson', 
+      author_email='dotsdl@gmail.com',
+      url='http://mdsynthesis.readthedocs.org/',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
       packages=find_packages('src'),
       package_dir={'': 'src'},
       license='GPL 2',
@@ -20,6 +32,6 @@ setup(name='mdsynthesis',
       install_requires=[
                 'datreant.core>=0.6.0',
                 'datreant.data>=0.6.0',
-                'MDAnalysis>=0.11.0',
+                'MDAnalysis>=0.14.0',
                 ],
      )
--- src/mdsynthesis/__init__.py
+++ src/mdsynthesis/__init__.py
@@ -6,17 +6,6 @@
 """
 MDSynthesis --- a persistence engine for molecular dynamics data
 ================================================================
-
-MDSynthesis is designed to address the logistical aspects of molecular dynamics
-trajectory analysis. Whereas MDAnalysis gives the computational tools to
-dissect trajectories, MDSynthesis provides a framework for automatically
-organizing the results. This allows you (the scientist) to focus on your
-science, letting the computer handle the lower-level logistical details.
-
-.. SeeAlso:: :class:`mdsynthesis.treants.Sim`
-             :class:`mdsynthesis.treants.Group`
-
-
 """
 # Bring some often used objects into the current namespace
 from datreant.core import Treant, Group, Bundle, Tree, Leaf, View
