0ccc61c8f14167ffcf0d9d3c8f063dc737f661a1
David Dotson <dotsdl@gmail.com>
2016-05-16 23:15:56 -0700

    Release 0.6.1.
    This is a minor bugfix/performance improvement release.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ For a basic installation just type the command::
 from setuptools import setup, find_packages
 
 setup(name='mdsynthesis',
-      version='0.7.0-dev',
+      version='0.6.1',
       description='a persistence engine for molecular dynamics data',
       author='David Dotson', 
       author_email='dotsdl@gmail.com',
