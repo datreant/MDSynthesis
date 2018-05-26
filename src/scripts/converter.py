b0b7253d2ca681dbd2d8142dee28c4d4846cc755
Max Linke <max_linke@gmx.de>
2018-05-26 18:31:34 +0200

    also update universe definition and atomselection

--- src/scripts/converter.py
+++ src/scripts/converter.py
@@ -1,20 +1,45 @@
 #! /usr/bin/env python
 import mdsynthesis as mds
+import MDAnalysis as mda
 import json
 from os import path
 from glob import glob
+import six
+
 
 def convert(folder):
     json_file = glob(path.join(folder, 'Sim*'))[0]
     with open(json_file) as fh:
         old = json.load(fh)
-    mds.Sim(folder, categories=old['categories'], tags=old['tags'])
+    sim = mds.Sim(folder, categories=old['categories'], tags=old['tags'])
+
+    old_sim = old['mdsynthesis']
+    # update universe definition
+    udef = old_sim['universedef']
+    if udef['topology']:
+        args = [udef['topology'],]
+        if udef['trajectory']:
+            args.append(udef['trajectory'])
+        u = mda.Universe(*args, **udef['kwargs'])
+        sim.universe = u
+
+    # update atom selection
+    atom_sel = old_sim['atomselections']
+    for key, val in six.iteritems(atom_sel):
+        sim.atomselections[key] = val
+
 
 if __name__ == '__main__':
     import argparse
-    parser = argparse.ArgumentParser(description="Convert old sims to new sims.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("directories", metavar="DIRECTORY", nargs="+", help="one or more directories that are being processed")
+    parser = argparse.ArgumentParser(
+        description="Convert old sims to new sims.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "directories",
+        metavar="DIRECTORY",
+        nargs="+",
+        help="one or more directories that are being processed")
     args = parser.parse_args()
 
-for dir in args.directories:
-    convert(dir)
+    for dir in args.directories:
+        convert(dir)
